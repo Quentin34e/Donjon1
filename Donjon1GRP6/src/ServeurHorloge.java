6f3ab78a143bb132d0b4d957a8e71160e386d230
QuentinLe77 <QuentinLe77@192.168.1.101>
2021-03-26 17:26:33 +0100

    j

---
+++ Donjon1GRP6/src/ServeurHorloge.java
@@ -0,0 +1,58 @@
+package Donjon1GRP6.src;
+
+import java.io.*;
+import java.net.*;
+
+public class ServeurHorloge extends Thread{
+	
+
+	private Socket socket;
+	private PrintStream out;
+	private BufferedReader in;
+	
+	public ServeurHorloge(Socket s){
+		this.socket = s;
+	}
+	
+	
+	public void run(){
+		try {
+				in = new BufferedReader(
+			          new InputStreamReader(socket.getInputStream())
+			          );
+				
+				out = new PrintStream(socket.getOutputStream());
+				
+				//envoi du premier message
+				out.println("SERVEUR : en attente de requête");
+				
+				
+				//5 requêtes
+			for(int req=0;req<5;req++){
+				
+			
+				String message = in.readLine();
+			
+				if (message.equals("heure")){
+					
+					out.println(System.nanoTime());//+" "+req);
+			
+				}
+				else
+				{  
+					
+					out.println("bug");//+req);
+				}
+			}
+			socket.close();
+			
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
